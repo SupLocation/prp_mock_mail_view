182dab50ca63008c5c6398f2938fdcba097bb093
yuya-morimoto <my.hack.0312@gmail.com>
2025-02-19 23:19:25 +0900

    add: imap, pop component

---
+++ src/Pop.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@chakra-ui/react";
+
+export const Pop = () => {
+  return (
+    <Box
+      style={{
+        width: "50vw",
+        padding: "16px",
+      }}
+    >
+      POP
+    </Box>
+  );
+};
